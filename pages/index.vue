cf77a86a80206bfa94d15524978c17e19545f061
LeeShannon <leeshannong@hotmail.com>
2018-05-19 14:44:32 +0200

    login styling

--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,7 @@
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
+import Login from './login.vue'
 
 export default {
   components: {
--- pages/login.vue
+++ pages/login.vue
@@ -1,43 +1,78 @@
 <template>
-  <section class="container">
-    <h1>Login page</h1>
+  <section class="login-flex">
+    <h1 class="header">Login</h1>
+      <div class="login-form">
+        <form class="" action="index.html" method="post">
+          <label for="">Username</label>
+          <input type="text" name="" value="" class="login-input">
+          <label for="">Password</label>
+          <input type="password" name="" value=""  class="login-input">
+          <div class="" align="center">
+            <button class="login-button" type="button" name="button">Submit</button>
+          </div>
+
+        </form>
+      </div>
   </section>
 </template>
 
 <script>
 
 export default {
-  
+
 }
+
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
+<style scoped>
+.login-flex {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  text-align: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.header{
+  font-family: sans-serif;
+  font-weight: 400;
+  font-size: 2.0em;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.login-form {
+  width: 30%;
+  height: auto;
+  background-color: white;
+  margin: 3% auto 100px;
+  padding: 45px;
+  border-radius: 5%;
+  /* box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.1), 0 5px 5px 0 rgba(0, 0, 0, 0.1); */
 }
 
-.links {
-  padding-top: 15px;
+.login-input {
+  width: 100%;
+  font-size: 15px;
+  margin: 0 0 15px;
+  border: none;
+  border-bottom: 1px solid #7f8184;
+  padding-bottom: 7px;
+}
+
+.login-button {
+  /* position: relative; */
+  padding: 10px 10px;
+  border-radius: 10px;
+  font-family: sans-serif;
+  font-size: 1.0em;
+  color: #FFF;
+  text-decoration: none;
+  border: 0;
+  background-color: #80CBC4;
+  cursor: pointer;
+}
+
+.login-button:hover {
+  background-color: #4f9a94;
 }
 </style>
